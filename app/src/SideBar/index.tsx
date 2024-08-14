ac36dab00cae335fe9839aac2e425eb8db67d56d
juangh27 <101841901+juangh27@users.noreply.github.com>
2024-08-14 17:12:59 -0600

    clean up for first build

--- app/src/SideBar/index.tsx
+++ app/src/SideBar/index.tsx
@@ -20,7 +20,6 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import { ListItems } from './listItems';
 import { Outlet } from 'react-router-dom';
-import StickyFooter from '../Footer';
 
 const drawerWidth = 240;
 
